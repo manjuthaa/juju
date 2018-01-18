b01de7b30ca6632df68a8aef754193ac350e1954
manjuthaa <31506174+manjuthaa@users.noreply.github.com>
2018-01-18 11:26:15 +0000

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(0
+{
+char c;
+int a,b;
+printf("enter the alphabet\n");
+scanf("%c",c==&c);
+a=(c=='a'||c=='e'||c=='i'||c=='o'||c=='u');
+b=(c=='A'||c=='E'||c=='I'||c=='O'||c=='U');
+if(a||b)
+printf("%c is vowel",c);
+else
+printf("%c is aconstant",c);
+return 0;
+}
